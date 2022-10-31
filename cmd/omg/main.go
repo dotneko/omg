d0b5c719b82288dd4e439b8e61ede9390e92834a
dotneko <4761762+dotneko@users.noreply.github.com>
2022-10-31 15:12:01 +0800

    use flags

--- cmd/omg/main.go
+++ cmd/omg/main.go
@@ -1,48 +1,62 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "strings"
+	"flag"
+	"fmt"
+	"os"
 
-  "omg"
+	//"strings"
+
+	"omg"
 )
 
 // Set filename
 const omgFileName = ".omg.json"
 
 func main() {
-  // Define an wallet list
-  l := &omg.Wallets{}
+	// Parsing command line flags
+	add := flag.String("add", "", "Add [account_name] [address] to wallets list")
+	list := flag.Bool("list", false, "List all accounts")
+	flag.Parse()
+	tail := flag.Args()
 
-  // Read items from file
-  if err := l.Get(omgFileName); err != nil {
-    fmt.Fprintln(os.Stderr, err)
-    os.Exit(1)
-  }
+	// Define an wallet list
+	l := &omg.Wallets{}
 
-  // Decide what to do based on number of arguments provided
-  switch {
-    case len(os.Args) == 2 && strings.ToLower(os.Args[1]) == "list":
-      count := 0
-      for _, account := range *l {
-        fmt.Println(account.Name, account.Address)
-        count++
-      }
-      if count == 0 {
-        fmt.Println("No accounts in store")
-      }
-    case len(os.Args) == 4 && strings.ToLower(os.Args[1]) == "add":
-        name := os.Args[2]
-        address := os.Args[3]
-        l.Add(name, address)
-        // Save the new list
-        if err := l.Save(omgFileName); err != nil {
-          fmt.Fprintln(os.Stderr, err)
-          os.Exit(1)
-        }
-        fmt.Printf("Added %q, %q to wallets\n", name, address)
-    default:
-      fmt.Printf("Received %d args; Args: %q\n", len(os.Args), os.Args[1:])
-  }
+	// Read items from file
+	if err := l.Get(omgFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	// Decide what to do based on number of arguments provided
+	switch {
+	case *list:
+		count := 0
+		for _, account := range *l {
+			fmt.Println(account.Name, account.Address)
+			count++
+		}
+		if count == 0 {
+			fmt.Println("No accounts in store")
+		}
+	case *add != "":
+		// Get 1st string of remaining arguments as address
+		if len(tail) == 0 {
+			fmt.Println("Error: no address entered")
+			os.Exit(0)
+		}
+		address := tail[0]
+		fmt.Printf("Address for %q : ", address)
+
+		l.Add(*add, address)
+		// Save the new list
+		if err := l.Save(omgFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		fmt.Printf("Added %q, %q to wallets\n", *add, address)
+	default:
+		fmt.Printf("Received %d args; Args: %q\n", len(os.Args), os.Args[1:])
+	}
 }
--- cmd/omg/main_test.go
+++ cmd/omg/main_test.go
@@ -1,66 +1,68 @@
 package main_test
 
 import (
-  "fmt"
-  "os"
-  "os/exec"
-  "path/filepath"
-  "runtime"
-  "strings"
-  "testing"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"testing"
 )
 
 var (
-  binName = "omg"
-  fileName = ".omg.json"
+	binName  = "omg"
+	fileName = ".omg.json"
 )
 
 func TestMain(m *testing.M) {
-  fmt.Println("Building tool...")
-  if runtime.GOOS == "windows" {
-    binName += ".exe"
-  }
+	fmt.Println("Building tool...")
+	if runtime.GOOS == "windows" {
+		binName += ".exe"
+	}
 
-  build := exec.Command("go", "build", "-o", binName)
-  if err := build.Run(); err != nil {
-    fmt.Fprintf(os.Stderr, "Cannot build tool %s: %s", binName, err)
-    os.Exit(1)
-  }
-  fmt.Println("Running tests...")
-  result := m.Run()
-  fmt.Println("Cleaning up...")
-  os.Remove(binName)
-  os.Remove(fileName)
-  os.Exit(result)
+	build := exec.Command("go", "build", "-o", binName)
+	if err := build.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot build tool %s: %s", binName, err)
+		os.Exit(1)
+	}
+	fmt.Println("Running tests...")
+	result := m.Run()
+	fmt.Println("Cleaning up...")
+	os.Remove(binName)
+	os.Remove(fileName)
+	os.Exit(result)
 }
 
 func TestOmgCLI(t *testing.T) {
-  //a := omg.Account{Name: "Test1", Address: "Address1"}
-  aNameAddress := "Test1 Address1"
-  dir, err := os.Getwd()
-  if err != nil {
-    t.Fatal(err)
-  }
-  cmdPath := filepath.Join(dir, binName)
+	//a := omg.Account{Name: "Test1", Address: "Address1"}
 
-  t.Run("AddNewAccount", func(t *testing.T) {
-    cmd := exec.Command(cmdPath, strings.Split("add " + aNameAddress, " ")...)
-    if err := cmd.Run(); err != nil {
-      t.Fatal(err)
-    }
-  })
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	cmdPath := filepath.Join(dir, binName)
+	aNameAddress := "Test1 Address1"
 
-  t.Run("ListWallets", func(t *testing.T) {
-    cmd := exec.Command(cmdPath,"list")
-    out, err := cmd.CombinedOutput()
-    if err != nil {
-      t.Fatal(err)
-    }
-    
-    expected := aNameAddress + "\n"
+	t.Run("AddNewAccount", func(t *testing.T) {
 
-    if expected != string(out) {
-      t.Errorf("Expected %q, got %q instead\n", expected, string(out))
-    }
-  })
+		cmd := exec.Command(cmdPath, strings.Split("-add "+aNameAddress, " ")...)
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("ListWallets", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-list")
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		expected := aNameAddress + "\n"
+
+		if expected != string(out) {
+			t.Errorf("Expected %q, got %q instead\n", expected, string(out))
+		}
+	})
 }
