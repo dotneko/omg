52f48349ee2472ee64985085cc3435ee0923a708
dotneko <4761762+dotneko@users.noreply.github.com>
2022-11-20 07:54:54 +0800

    Apply pretty formatting

--- app/controller.go
+++ app/controller.go
@@ -132,7 +132,7 @@ func CheckBalances(address string) {
 	if err != nil {
 		fmt.Sprintln(err)
 	}
-	fmt.Printf("Avaliable balance : %s%s (%8.5f %s)\n", PrettifyDenom(balance), cfg.Denom, DenomToToken(balance), cfg.Token)
+	fmt.Printf("Avaliable balance : %s %s (%8.5f %s)\n", PrettifyDenom(balance), cfg.Denom, DenomToToken(balance), cfg.Token)
 }
 
 // Get keyring name and addresses
